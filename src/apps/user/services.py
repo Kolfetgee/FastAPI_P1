f7709fdafb75459a6ed21a98e4b973d129dcbd5f
Kolfetgee <nikolay_shevtsov@list.ru>
2026-04-01 21:46:35 +0300

    add service layer skeleton

--- src/apps/user/services.py
+++ src/apps/user/services.py
@@ -1,9 +1,22 @@
 from src.apps.user.repository import UserRepository
+from src.apps.user.routers import user_service
+from src.apps.user.schemas import UserRead, UserCreate
 
 
 class UserService:
     def __init__(self, repository: UserRepository) -> None:
         self.repository = repository
 
-    def get_all_users(self) -> list[dict]:
-        return self.repository.get_all()
+
+
+    def get_user(self, user_id: int) -> UserRead | None:
+        self.repository.get_by_id(user_id)
+
+    def get_users(self) -> list[UserRead]:
+        return  self.repository.get_all()
+
+    def create_user(self, user_create: UserCreate) -> UserRead:
+        return self.repository.create(user_create)
+
+
+user_service = UserService(repository=UserRepository())
