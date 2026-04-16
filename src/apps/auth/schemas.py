db3f8179150c4971b5b93625a6cf483165dd3659
Nikolay <kolyan1590@gmail.com>
2026-04-16 10:56:26 +0300

    align auth password validation

--- src/apps/auth/schemas.py
+++ src/apps/auth/schemas.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel, EmailStr, Field
 class RegisterRequest(BaseModel):
     username: str = Field(min_length=3, max_length=12)
     email: EmailStr
-    password: str = Field(min_length=6, max_length=10)
+    password: str = Field(min_length=6, max_length=50)
 
 
 class LoginRequest(BaseModel):
     email: EmailStr
-    password: str = Field(min_length=6, max_length=10)
+    password: str = Field(min_length=6, max_length=50)
 
 
 class RefreshTokenRequest(BaseModel):
